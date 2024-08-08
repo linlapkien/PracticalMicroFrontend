ab93af0856954bdda4beccd28142e04b31163941
Lin Lập Kiến <linlapkien@gmail.com>
2024-08-08 15:54:49 +0700

    first commit

---
+++ nextjs-mf-apps/shop-app/next.config.mjs
@@ -0,0 +1,26 @@
+import NextFederationPlugin from '@module-federation/nextjs-mf';
+
+const nextConfig = {
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'shop_app',
+        remotes: {},
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {
+          './Products': './components/Products.js',
+        },
+        extraOptions: {
+          debug: false, // `false` by default
+          exposePages: false, // `false` by default
+        },
+        shared: {},
+      })
+    );
+
+    return config;
+  },
+};
+
+export default nextConfig;
